370a0de1825ee299a68819bb33d4a7aa9d434ccf
Jason Huber <jasonhuber@gmail.com>
2015-03-17 22:57:47 -0700

    So complex. Fixed a few things
    Got some things fixed for a demo…

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,15 @@
 //The JS that does the timer stuff.
-function starttimer(numsec)
+var timeoutfunct;
+function starttimer(numsec, numarrows, container)
 {
-  alert(numsec +"!!");
-  debugger;
+  $(container).html(numsec*numarrows);
+  timeoutfunct = setInterval(function(){changetime(container)}, 1000);
+}
 
+function changetime(container)
+{
+  $(container).html($(container).html()-1);
+  if ($(container).html() <= 0) {
+    clearTimeout(timeoutfunct);
+  }
 }
